cfe6c317f8322d08af79f73913e79e7c36812b71
Darkhax <darklime@live.ca>
2016-01-07 05:12:16 -0700

    Atempt to make the textures work.

--- src/java/net/darkhax/deadmau5hats/Deadmau5Hats.java
+++ src/java/net/darkhax/deadmau5hats/Deadmau5Hats.java
@@ -1,11 +1,22 @@
 package net.darkhax.deadmau5hats;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import net.darkhax.deadmau5hats.client.renderer.Deadmau5MeshDefinition;
 import net.darkhax.deadmau5hats.common.ProxyCommon;
+import net.darkhax.deadmau5hats.item.ItemDeadmau5Helmet;
 import net.darkhax.deadmau5hats.libs.Constants;
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
 
 @Mod(modid = Constants.MOD_ID, name = Constants.MOD_NAME, version = Constants.VERSION_NUMBER, acceptedMinecraftVersions = Constants.MCVERSION)
 public class Deadmau5Hats {
@@ -16,8 +27,29 @@ public class Deadmau5Hats {
     @Mod.Instance(Constants.MOD_ID)
     public static Deadmau5Hats instance;
     
-    @EventHandler
-    public void preInit (FMLPreInitializationEvent pre) {
+    public static Item item;
     
+    @EventHandler
+    public void preInit (FMLPreInitializationEvent event) {
+        
+        item = new ItemDeadmau5Helmet();
+        GameRegistry.registerItem(item, "deadmau5hat");
+    }
+    
+    @EventHandler
+    public void init (FMLInitializationEvent event) {
+        
+        if (event.getSide().equals(Side.CLIENT)) {
+            
+            List<ModelResourceLocation> models = new ArrayList<ModelResourceLocation>();
+            
+            for (String varient : ItemDeadmau5Helmet.variants)
+                models.add(new ModelResourceLocation("deadmau5hats:deadmau5hat_" + varient, "inventory"));
+                
+            // ModelLoader.registerItemVariants(item, models.toArray(new
+            // ModelResourceLocation[models.size()]));
+            ModelLoader.registerItemVariants(item, new ModelResourceLocation("deadmau5hats:deadmau5hat_Black"));
+            ModelLoader.setCustomMeshDefinition(item, new Deadmau5MeshDefinition());
+        }
     }
 }
